76b12d705e336cea554925d1f575de28d289720a
John Walsh <john.walsh16609@gmail.com>
2015-11-18 03:07:12 +0000

    Added Missing Files
    Added new files missing...

---
+++ Assets/Scripts/ClaimHighScore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClaimHighScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//ClaimScore ();
+	
+	}
+	static void ClaimScore (){
+		GlobalScoreBoard g = new GlobalScoreBoard ();
+		if (g.isHighScore (700)) {
+			//check if user current score > the one saved in player pref then only claim the highScore..
+			g.ClaimCurrentScore ("Wow Score", 99999);
+		} else {
+			Debug.Log("Not in top 5 ");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ScoreboardView.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+public class ScoreboardView : MonoBehaviour {
+
+
+    public Text txt;
+	string scores = " ";
+	string newline="\n";
+	bool isPrinted = false;
+	
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI() {
+		if (GlobalScoreBoard.topScores.Count == 5 &&!isPrinted) {
+			printScores();
+			isPrinted=true;
+			Debug.Log("Executed/ran  once");
+		}
+	}
+
+	public void printScores(){
+
+		string player1 = newline+ GlobalScoreBoard.topScores.ElementAt(0).playerName + "   " +GlobalScoreBoard.topScores.ElementAt(0).PlayerScore + newline;
+		string player2 = GlobalScoreBoard.topScores.ElementAt(1).playerName + "   " + GlobalScoreBoard.topScores.ElementAt(1).PlayerScore + newline;
+		string player3 = GlobalScoreBoard.topScores.ElementAt(2).playerName + "   " + GlobalScoreBoard.topScores.ElementAt(2).PlayerScore + newline;
+		string player4 = GlobalScoreBoard.topScores.ElementAt(3).playerName + "   " + GlobalScoreBoard.topScores.ElementAt(3).PlayerScore + newline;
+		string player5 = GlobalScoreBoard.topScores.ElementAt(4).playerName + "   " + GlobalScoreBoard.topScores.ElementAt(4).PlayerScore + newline;
+		txt.text = player1 + player2 + player3 + player4 + player5;
+		
+	}
+	
+}
