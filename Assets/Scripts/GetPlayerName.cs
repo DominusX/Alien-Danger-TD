6bf37e0ceafba9975db457c9b2927cb22c2b242d
John Walsh <john.walsh16609@gmail.com>
2015-12-16 02:08:03 +0000

    Adding Missing Files
    Again github forgot some files....

---
+++ Assets/Scripts/GetPlayerName.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetPlayerName : MonoBehaviour {
+	
+	public InputField nameInputField;
+	
+	private void Start(){
+		InputField.SubmitEvent submitEvent = new InputField.SubmitEvent();
+		submitEvent.AddListener(SubmitName);
+		nameInputField.onEndEdit = submitEvent;
+	}
+	
+	private void SubmitName(string playerName){
+		PlayerPrefs.SetString("Player Name", playerName);
+	}
+}
