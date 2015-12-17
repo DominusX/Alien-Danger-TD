fea0f58153eaf07e9fbe87a7ef14a3fd1bb7b760
Arjun Kharel <adgn353@gmail.com>
2015-12-17 21:44:12 +0000

    Global Score for windows store apps added

--- Assets/Scripts/HighScoresWindows.cs
+++ Assets/Scripts/HighScoresWindows.cs
@@ -1,34 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class WindowsHighscoresWindows : MonoBehaviour {
+public class HighScoresWindows : MonoBehaviour
+{
 
     const string privateCode = "_o9I4WdLj0SOrleD45fnVwyIg3pEOeWUirsvAUDATK4w";
     const string publicCode = "5672ddb36e51b608e4448b8e";
     const string webURL = "http://dreamlo.com/lb/";
+    public Text scoreText;
 
+  
     public Highscore[] highscoresList;
-    public static WindowsHighscoresWindows instance;
+    public static HighScoresWindows instance;
+    
+    public int SCORE_MAX = 5;
+
+    void Start()
+    {
+        
+        DownloadHighscores();
+        
+      // AddNewHighscore("Harry", 1500);
+    }
 
     void Awake()
     {
+        
         instance = this;
     }
-    // Use this for initialization
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     public static void AddNewHighscore(string username, int score)
     {
         instance.StartCoroutine(instance.UploadNewHighscore(username, score));
     }
+
     IEnumerator UploadNewHighscore(string username, int score)
     {
         WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
@@ -43,27 +49,48 @@ public class WindowsHighscoresWindows : MonoBehaviour {
             print("Error uploading: " + www.error);
         }
     }
+
     public void DownloadHighscores()
     {
         StartCoroutine("DownloadHighscoresFromDatabase");
     }
 
+    IEnumerator DownloadHighscoresFromDatabase()
+    {
+        WWW www = new WWW(webURL + publicCode + "/pipe/");
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            FormatHighscores(www.text);
+          
+        }
+        else {
+            print("Error Downloading: " + www.error);
+        }
+    }
+
     void FormatHighscores(string textStream)
     {
+         string tempScore = " ";
         string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
         highscoresList = new Highscore[entries.Length];
-
-        for (int i = 0; i < entries.Length; i++)
+        
+        for (int i = 0; i < SCORE_MAX; i++)
         {
             string[] entryInfo = entries[i].Split(new char[] { '|' });
             string username = entryInfo[0];
             int score = int.Parse(entryInfo[1]);
             highscoresList[i] = new Highscore(username, score);
-            print(highscoresList[i].username + ": " + highscoresList[i].score);
+            
+            tempScore+= highscoresList[i].username + ": " + highscoresList[i].score +"\n" ;
+           
         }
+        scoreText.text = tempScore;
     }
-   
+
 }
+
 public struct Highscore
 {
     public string username;
---
+++ Assets/Scripts/WindowsClameScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowsClameScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       
+        HighScoresWindows.AddNewHighscore("JohnWalsh", 2000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
