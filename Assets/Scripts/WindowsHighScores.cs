5c6b97e082a1782d00e45dd331126d0b6f4e3a28
Arjun Kharel <adgn353@gmail.com>
2015-12-17 19:08:42 +0000

    Windows Scoreboard initial test

---
+++ Assets/Scripts/WindowsHighScores.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowsHighscoresWindows : MonoBehaviour {
+
+    const string privateCode = "_o9I4WdLj0SOrleD45fnVwyIg3pEOeWUirsvAUDATK4w";
+    const string publicCode = "5672ddb36e51b608e4448b8e";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    public Highscore[] highscoresList;
+    public static WindowsHighscoresWindows instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public static void AddNewHighscore(string username, int score)
+    {
+        instance.StartCoroutine(instance.UploadNewHighscore(username, score));
+    }
+    IEnumerator UploadNewHighscore(string username, int score)
+    {
+        WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            print("Upload Successful");
+            DownloadHighscores();
+        }
+        else {
+            print("Error uploading: " + www.error);
+        }
+    }
+    public void DownloadHighscores()
+    {
+        StartCoroutine("DownloadHighscoresFromDatabase");
+    }
+
+    void FormatHighscores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoresList = new Highscore[entries.Length];
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            highscoresList[i] = new Highscore(username, score);
+            print(highscoresList[i].username + ": " + highscoresList[i].score);
+        }
+    }
+   
+}
+public struct Highscore
+{
+    public string username;
+    public int score;
+
+    public Highscore(string _username, int _score)
+    {
+        username = _username;
+        score = _score;
+    }
+
+}
